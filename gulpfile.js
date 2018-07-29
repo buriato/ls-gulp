3716566f004bf0cda0271dd1e670511098832bce
buriato <bair.ungo@yandex.com>
2018-07-29 15:26:31 +0200

    added some mixins etc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,89 @@
-const gulp = require('gulp');
-const pug = require('gulp-pug');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cssunit = require('gulp-css-unit');
+const gulp = require("gulp");
+const pug = require("gulp-pug");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cssunit = require("gulp-css-unit");
 const notify = require("gulp-notify");
-const rename = require('gulp-rename');
-const sourcemaps = require('gulp-sourcemaps');
-const del = require('del');
-const browserSync = require('browser-sync');
-const gulpWebpack = require('gulp-webpack');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js');
+const rename = require("gulp-rename");
+const sourcemaps = require("gulp-sourcemaps");
+const del = require("del");
+const browserSync = require("browser-sync");
+const gulpWebpack = require("gulp-webpack");
+const webpack = require("webpack");
+const webpackConfig = require("./webpack.config.js");
 
 const paths = {
-  root: './build',
+  root: "./build",
   templates: {
-    pages: 'src/templates/pages/*.pug',
-    src: 'src/templates/**/*.pug'
+    pages: "src/templates/pages/*.pug",
+    src: "src/templates/**/*.pug"
   },
   styles: {
-    src: 'src/styles/**/*.sass',
-    dest: 'build/assets/styles'
+    src: "src/styles/**/*.sass",
+    dest: "build/assets/styles"
   },
   images: {
-    src: 'src/images/**/*.*',
-    dest: 'build/assets/images/'
+    src: "src/images/**/*.*",
+    dest: "build/assets/images/"
+  },
+  fonts: {
+    src: "src/fonts/**/*.*",
+    dest: "build/assets/fonts/"
   },
   scripts: {
-    src: 'src/scripts/**/*.js',
-    dest: 'build/assets/scripts'
+    src: "src/scripts/**/*.js",
+    dest: "build/assets/scripts"
   }
-}
+};
 
 //pug
 
 function templates() {
-  return gulp.src(paths.templates.pages)
-    .pipe(pug({
-      pretty: true
-    }))
-    .on('error', notify.onError(function (error) {
-      return {
-        title: 'Pug',
-        message: error.message
-      };
-    }))
+  return gulp
+    .src(paths.templates.pages)
+    .pipe(
+      pug({
+        pretty: true
+      })
+    )
+    .on(
+      "error",
+      notify.onError(function (error) {
+        return {
+          title: "Pug",
+          message: error.message
+        };
+      })
+    )
     .pipe(gulp.dest(paths.root));
 }
-
 
 //sass
 
 function styles() {
-  return gulp.src('./src/styles/app.sass')
+  return gulp
+    .src("./src/styles/app.sass")
     .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: 'expanded'
-    })).on("error", notify.onError())
-    .pipe(cssunit({
-      type: 'px-to-rem',
-      rootSize: 16
-    }))
-    .pipe(autoprefixer(['last 15 versions']))
+    .pipe(
+      sass({
+        outputStyle: "expanded"
+      })
+    )
+    .on("error", notify.onError())
+    .pipe(
+      cssunit({
+        type: "px-to-rem",
+        rootSize: 16
+      })
+    )
+    .pipe(autoprefixer(["last 15 versions"]))
     .pipe(sourcemaps.write())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(gulp.dest(paths.styles.dest));
 }
 
 //clean
@@ -78,7 +95,8 @@ function clean() {
 //webpack
 
 function scripts() {
-  return gulp.src('src/scripts/app.js')
+  return gulp
+    .src("src/scripts/app.js")
     .pipe(gulpWebpack(webpackConfig, webpack))
     .pipe(gulp.dest(paths.scripts.dest));
 }
@@ -99,14 +117,19 @@ function server() {
     server: paths.root,
     open: false
   });
-  browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+  browserSync.watch(paths.root + "/**/*.*", browserSync.reload);
+}
+
+//move fonts
+
+function fonts() {
+  return gulp.src(paths.fonts.src).pipe(gulp.dest(paths.fonts.dest));
 }
 
 //move images
 
 function images() {
-  return gulp.src(paths.images.src)
-    .pipe(gulp.dest(paths.images.dest));
+  return gulp.src(paths.images.src).pipe(gulp.dest(paths.images.dest));
 }
 
 exports.templates = templates;
@@ -114,8 +137,11 @@ exports.styles = styles;
 exports.clean = clean;
 exports.images = images;
 
-gulp.task('default', gulp.series(
-  clean,
-  gulp.parallel(styles, templates, images, scripts),
-  gulp.parallel(watch, server)
-));
+gulp.task(
+  "default",
+  gulp.series(
+    clean,
+    gulp.parallel(styles, templates, images, fonts, scripts),
+    gulp.parallel(watch, server)
+  )
+);
